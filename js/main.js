814114f45668d9f16ac601b93078537354aff975
irungumargaret <sairmargaret44@gmail.com>
2019-11-29 13:29:41 +0300

    add an if else statement

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function akanname() 
+function akan() 
 {
     var maleNames =["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleNames =["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"];
@@ -8,11 +8,30 @@ function akanname()
     var month =parseInt(document.getElementByid("MM").value);
     var year =parseInt(document.getElementByid("YY").value);
 
-    var gender=document.genderSelector('input[name="gender"]:checked').value;
-   
+function invlid(day, month, year)
+        {
+        if ( day < 1 || day > 31) { 
+       
+          alert("Day entered is Invalid");
+            return false;
+        }
+        else if ( month < 1 || month > 12 || month == 2 && day > 29 ) {
+            alert("Month entered is invalid");
+            return false;
+        }
+        else if (year <1 || year >12) {
+            alert("Year entered is invalid");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+        }
+
    var day = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
-   
-   
+    var gender=document.genderSelector('input[name="gender"]:checked').value;
+function akanname (){
    if (gender ==1) {
        if (day ===0){
            alert("Hey + 'maleName + you were born on a Sunday");
@@ -59,4 +78,6 @@ function akanname()
             alert("Hey + 'femaleName + you were born on a Sartuday");
         }
     }
-   }
+  }
+}
+
